d5087815cdf701062d9d67e9ca40af3e334acc07
mohas8 <mdmobashirhasan@gmail.com>
2025-03-15 10:29:28 +0600

    s

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'screens/home/pages/home.dart';
+
+void main() {
+  runApp(const FreebeesEcom());
+}
+
+class FreebeesEcom extends StatelessWidget {
+  const FreebeesEcom({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Freebees E-com",
+      debugShowCheckedModeBanner: false,
+      home: Home(),
+    );
+  }
+}
